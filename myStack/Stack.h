ff19aca47094b4e42d7aec78b8e2843415498406
Goolos777 <Goolos777@gmail.com>
2014-01-17 03:28:59 +0200

    write stack.cpp

--- myStack/Stack.h
+++ myStack/Stack.h
@@ -1,8 +1,58 @@
+//1. реализовать безразмерный стэк
+
+
 #pragma once
+#include <string>
+using namespace std;
+template <class T>
 class Stack
 {
+	struct Node
+	{
+		Node *next;
+		T elemet;
+	};
+	Node *head;
 public:
-	Stack();
-	~Stack();
+	Stack() :head(nullptr){}
+	~Stack()
+	{
+		Node tmp = head;
+		while (head)
+		{
+			//выт€гиваем и удал€ем элемент
+		}
+	}
+	// установить в стек
+	void push(T val)
+	{
+		Note* tmp;
+		tmp = new Node;
+		tmp->elem = val;
+		tmp->next = nullptr;
+		if (head == nulptr)
+		{
+			head = tmp;
+			return;
+		}
+		Note* tmp2 = head;
+		while (true)
+		{
+			if (tmp2->next == nullptr)
+			{
+				tmp2->next = tmp;
+				return;
+			}
+		}
+	}
+	//достать из стека
+	T top()
+	{
+		
+
+
+	}
+	void pop();
+	void print();
 };
 
